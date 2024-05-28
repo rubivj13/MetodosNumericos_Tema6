872c1f883ee00d02c70a1232369f860fd1733d77
Rubi Veloz Jimenez <147438464+rubivj13@users.noreply.github.com>
2024-05-27 18:34:04 -0600

    Update Ejercicio1.java

---
+++ Tema1/Overflow/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Ejemplo de desbordamiento en operaciones aritméticas con enteros
+        int a = Integer.MAX_VALUE; // Valor máximo para un entero
+        int b = 1; // Incremento en 1
+
+        // Intentamos incrementar el valor máximo de un entero
+        int suma = a + b;
+
+        // Imprimimos el resultado
+        System.out.println("Resultado de la suma: " + suma); // Se produce un desbordamiento
+    }
+}
